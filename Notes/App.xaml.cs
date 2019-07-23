0c534ac7819875d0952a89156bb1386f35f2b308
FedeWax <cha_lino_23@hotmail.com>
2019-07-22 20:17:15 -0700

    primer commit

---
+++ Notes/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+
+namespace Notes
+{
+    public partial class App : Application
+    {
+        public static string FolderPath { get; private set; }
+
+        public App()
+        {
+            InitializeComponent(); 
+            //iniciializar la carpeta de la aplicacion donde estan guardados los datos
+            FolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData));
+            //indicar en que pagina comeinza la nagvecion
+            MainPage = new NavigationPage(new NotesPage());
+        }
+        // ...
+    }
+}
---
+++ Notes/NoteEntryPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Notes.Models;
+using System.IO;
+
+namespace Notes
+{
+    public partial class NoteEntryPage : ContentPage
+    {
+        public NoteEntryPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            var note = (Note)BindingContext;
+
+            if (string.IsNullOrWhiteSpace(note.Filename))
+            {
+                // Save generar un nombre aleatorio para el nombre 
+                var filename = Path.Combine(App.FolderPath, $"{Path.GetRandomFileName()}.notes.txt");
+                File.WriteAllText(filename, note.Text);
+            }
+            else
+            {
+                // Update
+                File.WriteAllText(note.Filename, note.Text);
+            }
+
+            await Navigation.PopAsync();
+        }
+
+        async void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            var note = (Note)BindingContext;
+
+            if (File.Exists(note.Filename))
+            {
+                File.Delete(note.Filename);
+            }
+            //con este metodo retornamos a la vista anterior
+            await Navigation.PopAsync();
+        }
+    }
+}
